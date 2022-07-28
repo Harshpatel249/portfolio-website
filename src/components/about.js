500c9af4c0c4ed133caabb7f55977be4cd7bb12c
Harsh Patel <harshpatel242000@gmail.com>
2022-07-28 18:05:44 +0530

    about section update

--- src/components/about.js
+++ src/components/about.js
@@ -1,9 +1,11 @@
 import * as React from "react";
 import './about.css';
+import Developer from '../assets/developer.svg';
 
 const About = () => (
     <div className="main justify-center">
-        <div className="container">
+        <div className="container justify-between">
+            <div>
             <p className="font['Poppins'] text-custom-green-500 mt-48 text-xl font-medium ml-1">Hello, I'm</p>
             <h1 className="font['Poppins'] text-custom-slate-200 mt-2 text-5xl font-medium">Harsh Patel.</h1>
             <h2 className="font['Poppins'] text-custom-slate-600 mt-4 text-3xl font-semibold">I aspire to build quality products <br />using code.</h2>
@@ -11,6 +13,8 @@ const About = () => (
             <button className="font['Poppins'] text-custom-green-500 text-sm font-normal mt-8 rounded-full border-2 border-color pl-8 pr-8 pt-2 pb-2">
                 Resume
             </button>
+            </div>
+            <img alt="Full-Stack developer" className="dev-img mt-48" src={Developer}></img>
         </div>
     </div>
 
