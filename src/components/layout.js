013e9cd3fba62cbfcea0ce00768887dd1a8794e1
Harsh Patel <harshpatel242000@gmail.com>
2022-09-16 23:17:53 +0530

    Added social media strip

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,11 +2,13 @@ import * as React from "react"
 import PropTypes from "prop-types"
 
 import Header from "./header"
+import SocialStrip from "./social-strip"
 import "./layout.css"
 
 const Layout = ({ children }) => {
   return (
     <>
+      <SocialStrip />
       <Header/>
       <main>{children}</main>
     </>
---
+++ src/components/social-strip.js
@@ -0,0 +1,34 @@
+import * as React from "react";
+import './social-strip.css';
+
+import InstagramIcon from '../assets/icon_insta.svg';
+import TwitterIcon from '../assets/icon_twitter.svg';
+import GithubIcon from '../assets/icon_github.svg';
+import LinkedinIcon from '../assets/icon_linkedin.svg';
+
+const SocialStrip = () => (
+    <div className="flex fixed flex-col items-start justify-center h-full pl-4">
+        <a href="https://www.instagram.com/harsh_249/" target="_blank">
+            <button>
+                <img alt="Instagram" className="w-12 hover:w-16" src={InstagramIcon}></img>
+            </button>
+        </a>
+        <a href="https://twitter.com/harshpatel_249" target="_blank">
+            <button>
+                <img alt="Twitter" className="w-12 mt-4 hover:w-16" src={TwitterIcon}></img>
+            </button>
+        </a>
+        <a href="https://github.com/Harshpatel249" target="_blank">
+            <button>
+                <img alt="Github" className="w-12 mt-4 hover:w-16" src={GithubIcon}></img>
+            </button>
+        </a>
+        <a href="https://www.linkedin.com/in/harsh-patel-696a82176/" target="_blank">
+            <button>
+                <img alt="Linkedin" className="w-12 mt-4 hover:w-16" src={LinkedinIcon}></img>
+            </button>
+        </a>
+    </div>
+)
+
+export default SocialStrip;
