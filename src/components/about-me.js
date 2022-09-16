2330ffb110f108a08ec616d189b409cc67f2357d
Harsh Patel <harshpatel242000@gmail.com>
2022-09-16 16:25:17 +0530

    json

---
+++ src/components/about-me.js
@@ -0,0 +1,16 @@
+import * as React from "react";
+import './about-me.css';
+
+const AboutMe = () => (
+    <div className="main justify-center mt-32">
+        <div className="container justify-between">
+            <div>
+                <h1 className="font['Poppins'] text-custom-slate-200 text-3xl font-medium">About Me</h1>
+            </div>
+            {/* <img alt="Full-Stack developer" className="dev-img mt-48" src={Developer}></img> */}
+        </div>
+    </div>
+
+);
+
+export default AboutMe;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
-import * as React from "react"
+import * as React from "react";
 
-import Layout from "../components/layout"
-import About from "../components/about"
+import Layout from "../components/layout";
+import About from "../components/about";
+import AboutMe from "../components/about-me";
+
 
 const IndexPage = () => (
   <Layout>
     <About/>
+    <AboutMe/>
   </Layout>
 )
 
