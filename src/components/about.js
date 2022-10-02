33cc170540e67665aadb233dd01e061736c0bf10
Harsh Patel <harshpatel242000@gmail.com>
2022-10-02 17:58:57 +0530

    updated CV

--- src/components/about.js
+++ src/components/about.js
@@ -17,7 +17,7 @@ const About = () => {
                     <h2 className="font['Poppins'] text-custom-slate-600 mt-2 ml-3 text-xl font-semibold">I aspire to build quality products using code.</h2>
                     <p className="font['Poppins'] text-custom-slate-900 mt-2 ml-3 mr-3 text-base font-normal">{Details.aboutp1}</p>
                 </div>
-                <a href="https://drive.google.com/file/d/1TUcOnSVTGxp6XOqrD9ce3MgUIXBfJG_b/view?usp=sharing" target="_blank" rel="noreferrer">
+                <a href="https://drive.google.com/file/d/1UtAekq49qccb-WRg6utEUJJ_BVKdTDz9/view?usp=sharing" target="_blank" rel="noreferrer">
                     <button className="font['Poppins'] text-custom-green-500 text-sm font-normal mt-8 rounded-full border-2 border-color pl-8 pr-8 pt-2 pb-2">
                         Resume
                     </button>
@@ -32,7 +32,7 @@ const About = () => {
                         <h1 className="font['Poppins'] text-custom-slate-200 mt-2 text-5xl font-medium">{Details.name}</h1>
                         <h2 className="font['Poppins'] text-custom-slate-600 mt-4 text-3xl font-semibold">I aspire to build quality products <br />using code.</h2>
                         <p className="font['Poppins'] text-custom-slate-900 mt-6 text-xl font-normal about-para">{Details.aboutp1}</p>
-                        <a href="https://drive.google.com/file/d/1TUcOnSVTGxp6XOqrD9ce3MgUIXBfJG_b/view?usp=sharing" target="_blank" rel="noreferrer">
+                        <a href="https://drive.google.com/file/d/1UtAekq49qccb-WRg6utEUJJ_BVKdTDz9/view?usp=sharing" target="_blank" rel="noreferrer">
                             <button className="font['Poppins'] text-custom-green-500 text-sm font-normal mt-8 rounded-full border-2 border-color pl-8 pr-8 pt-2 pb-2">
                                 Resume
                             </button>
